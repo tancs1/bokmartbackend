d8a98252f72042dc354fcfa68b329cc64becf2de
tancs1 <muhammadtanveerkamboh0@gmail.com>
2024-08-24 10:58:05 +0500

    bookmartjsonserverbackend

---
+++ index.js
@@ -0,0 +1,23 @@
+const jsonServer = require('json-server');
+const cors = require('cors'); // Import cors package
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+// Use CORS middleware
+
+const corsOptions = {
+    origin: 'http://your-frontend-domain.com', // Replace with your front-end domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  };
+  
+  server.use(cors(corsOptions));
+  
+server.use(middlewares);
+server.use(router);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
